3016df95255168ba7828465bae45a37f315b21af
HJ_Kim <50227588+HJK9810@users.noreply.github.com>
2022-10-18 16:23:00 +0900

    make domain

---
+++ src/main/java/spring/main/board/domain/BoardItems.java
@@ -0,0 +1,62 @@
+package spring.main.board.domain;
+
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class BoardItems {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long ID;
+    @Column
+    private String title;
+    @Column(columnDefinition = "DATE")
+    private Date date;
+    @Column(columnDefinition = "TEXT")
+    private String text;
+    @Column
+    private Long vieCnt;
+
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Long getVieCnt() {
+        return vieCnt;
+    }
+
+    public void setVieCnt(Long vieCnt) {
+        this.vieCnt = vieCnt;
+    }
+}
