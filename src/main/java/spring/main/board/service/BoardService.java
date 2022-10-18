613d03f0c06543f374962bcd30f4a2d204ad41ce
HJ_Kim <50227588+HJK9810@users.noreply.github.com>
2022-10-18 16:42:01 +0900

    add for use service

---
+++ src/main/java/spring/main/board/service/BoardService.java
@@ -0,0 +1,14 @@
+package spring.main.board.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import spring.main.board.domain.BoardItems;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BoardService {
+    Page<BoardItems> findAll(Pageable pageable);
+    Optional<BoardItems> findByID(Long id);
+    List<BoardItems> findAllByID(Long id);
+}
---
+++ src/main/java/spring/main/board/service/BoardServiceImpl.java
@@ -0,0 +1,33 @@
+package spring.main.board.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import spring.main.board.domain.BoardItems;
+import spring.main.board.repository.BoardRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BoardServiceImpl implements BoardService{
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Override
+    public Page<BoardItems> findAll(Pageable pageable) {
+        return boardRepository.findAll(pageable);
+    }
+
+    @Override
+    public Optional<BoardItems> findByID(Long id) {
+        return boardRepository.findByID(id);
+    }
+
+    @Override
+    public List<BoardItems> findAllByID(Long id) {
+        return boardRepository.findAllByID(id);
+    }
+}
